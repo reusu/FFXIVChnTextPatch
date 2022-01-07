c09c384937e0ff95d8fddebd3198e5467850e034
GpointChen <Gpoint.ghdkghdk@gmail.com>
2022-01-07 19:08:01 +0800

    1/7 Fixed logging and actionTransient.

--- src/main/java/name/yumao/ffxiv/chn/FFXIVPatchMain.java
+++ src/main/java/name/yumao/ffxiv/chn/FFXIVPatchMain.java
@@ -33,6 +33,7 @@ public class FFXIVPatchMain {
 		System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-7s] %5$s %n");
 		try {
 			FileHandler fh = new FileHandler("debug.log");
+			fh.setEncoding("UTF-8");
 			log.addHandler(fh);
 	        fh.setFormatter(new SimpleFormatter());  
 		} catch (IOException e) {  
